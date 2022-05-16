3ce01b9c47309bbbdf880efdc14b1324278ac875
Steven <s.f.lambregts@student.tudelft.nl>
2022-05-16 14:22:41 +0200

    Create plots for countermeasures

--- create_graphs.py
+++ create_graphs.py
@@ -74,7 +74,7 @@ def get_accuracy_subgraph(file_name, percentage):
 
 
 # Plot the results, depending on the datasets and output
-def plot(datasets, percentage=False, part='files'):
+def plot(datasets, percentage=False, part='files', countermeasures=False):
     # Create a figure with multiple axes
     fig, ax = plt.subplots()
 
@@ -83,50 +83,68 @@ def plot(datasets, percentage=False, part='files'):
 
     for dataset in datasets:
         # Our result file
-        file_name = f"accuracy_{dataset}.txt"
+        if countermeasures:
+            for c in [['padding']]:  # , ['obfuscation'], ['volume'], ['padding', 'volume']]:
+                countermeasure_prefix = ','.join(c)
+                file_name = f"accuracy_[{countermeasure_prefix}]_{dataset}.txt"
 
-        # Get the accuracy for our results
-        acc = get_accuracy(file_name, percentage)
+                # Get the accuracy for our results
+                acc = get_accuracy(file_name, percentage)
 
-        # Plot the files recovered
-        y = np.array([acc[key][f'average_{part}'] for key in x])
-        error = [acc[key][f'error_{part}'] for key in x]
+                # Plot the files recovered
+                y = np.array([acc[key][f'average_{part}'] for key in x])
+                error = [acc[key][f'error_{part}'] for key in x]
 
-        ax.plot(x, y, color=colors[dataset][0],
-                label=f'{prefix}recovered {part} {dataset.title() if not compare else "VAL"}')
-        ax.fill_between(x, np.clip(y - error, 0, 100) if percentage else y - error,
-                        np.clip(y + error, 0, 100) if percentage else y + error, alpha=0.5,
-                        edgecolor=colors[dataset][0], facecolor=colors[dataset][1])
+                ax.plot(x, y, color=colors[countermeasure_prefix][0],
+                        label=f'{prefix}recovered {part} {countermeasure_prefix}')
+                ax.fill_between(x, np.clip(y - error, 0, 100) if percentage else y - error,
+                                np.clip(y + error, 0, 100) if percentage else y + error, alpha=0.5,
+                                edgecolor=colors[countermeasure_prefix][0], facecolor=colors[countermeasure_prefix][1])
+        else:
+            file_name = f"accuracy_{dataset}.txt"
 
-        # Compare with the LEAP attack and the Subgraph_vol attack
-        if compare:
-            # LEAP
-            file_name = f"./leap/accuracy_{dataset}.txt"
+            # Get the accuracy for our results
             acc = get_accuracy(file_name, percentage)
 
             # Plot the files recovered
             y = np.array([acc[key][f'average_{part}'] for key in x])
             error = [acc[key][f'error_{part}'] for key in x]
 
-            ax.plot(x, y, color='#3F7F4C', label=f'{prefix}recovered {part} LEAP')
+            ax.plot(x, y, color=colors[dataset][0],
+                    label=f'{prefix}recovered {part} {dataset.title() if not compare else "VAL"}')
             ax.fill_between(x, np.clip(y - error, 0, 100) if percentage else y - error,
                             np.clip(y + error, 0, 100) if percentage else y + error, alpha=0.5,
-                            edgecolor='#3F7F4C', facecolor='#7EFF99')
+                            edgecolor=colors[dataset][0], facecolor=colors[dataset][1])
 
-            # The subgraph attack only recovers queries
-            if part == 'queries':
-                # Subgraph
-                file_name = f"./subgraph_vol/accuracy_{dataset}.txt"
-                acc = get_accuracy_subgraph(file_name, percentage)
+            # Compare with the LEAP attack and the Subgraph_vol attack
+            if compare:
+                # LEAP
+                file_name = f"./leap/accuracy_{dataset}.txt"
+                acc = get_accuracy(file_name, percentage)
 
-                # Plot the queries recovered
-                y = np.array([acc[key]['average_queries'] for key in x])
-                error = [acc[key]['error_queries'] for key in x]
+                # Plot the files recovered
+                y = np.array([acc[key][f'average_{part}'] for key in x])
+                error = [acc[key][f'error_{part}'] for key in x]
 
-                ax.plot(x, y, color='C1', label=f'{prefix}recovered {part} Subgraph_vol')
+                ax.plot(x, y, color='#3F7F4C', label=f'{prefix}recovered {part} LEAP')
                 ax.fill_between(x, np.clip(y - error, 0, 100) if percentage else y - error,
                                 np.clip(y + error, 0, 100) if percentage else y + error, alpha=0.5,
-                                edgecolor='#fff530', facecolor='#ede998')
+                                edgecolor='#3F7F4C', facecolor='#7EFF99')
+
+                # The subgraph attack only recovers queries
+                if part == 'queries':
+                    # Subgraph
+                    file_name = f"./subgraph_vol/accuracy_{dataset}.txt"
+                    acc = get_accuracy_subgraph(file_name, percentage)
+
+                    # Plot the queries recovered
+                    y = np.array([acc[key]['average_queries'] for key in x])
+                    error = [acc[key]['error_queries'] for key in x]
+
+                    ax.plot(x, y, color='C1', label=f'{prefix}recovered {part} Subgraph_vol')
+                    ax.fill_between(x, np.clip(y - error, 0, 100) if percentage else y - error,
+                                    np.clip(y + error, 0, 100) if percentage else y + error, alpha=0.5,
+                                    edgecolor='#fff530', facecolor='#ede998')
 
     ax.grid()
     ax.set_xlabel("Leakage (%)")
@@ -135,33 +153,51 @@ def plot(datasets, percentage=False, part='files'):
 
     ax.legend(loc='lower right')
 
-    title = f"Accuracy {'VAL ' if not compare else ''}{prefix}{part} recovered{' compared' if compare else ''}"
+    title = f"Accuracy {'VAL ' if not compare else ''}{prefix}{part} recovered" \
+            f"{' compared' if compare else ' countermeasures' if countermeasures else ''}"
     fig.suptitle(title)
     plt.tight_layout()
+    title = title.replace('%', 'percentage ').replace('#', 'nr ')
     plt.savefig(f'./plots/{title}', dpi=300, pad_inches=0)
     plt.show()
 
 
 if __name__ == '__main__':
-    compare = True
+    compare = False
     leaked_percentages = [0.1, 0.5, 1, 5, 10]
+    countermeasure = False
 
     colors = {'enron': ['#1B2ACC', '#089FFF'],
               'lucene': ['#3F7F4C', '#7EFF99'],
-              'wiki': ['#fff530', '#ede998']
+              'wiki': ['#FFF530', '#EDE998'],
+              'padding': ['#1B2ACC', '#089FFF'],
+              'obfuscation': ['#3F7F4C', '#7EFF99'],
+              'volume': ['#FFF530', '#EDE998'],
+              "padding,volume": ['F620FA', 'F6A8F7']
               }
 
-    if compare:
+    if countermeasure:
+        data = ['enron']
+
+        plot(data, part='files', countermeasures=countermeasure)
+        plot(data, True, 'files', countermeasures=countermeasure)
+
+        plot(data, part='queries', countermeasures=countermeasure)
+        plot(data, True, 'queries', countermeasures=countermeasure)
+    elif compare:
         leaked_percentages.append(30)
+        data = ['enron']
 
-        plot(['enron'], part='files')
-        plot(['enron'], True, 'files')
+        plot(data, part='files')
+        plot(data, True, 'files')
 
-        plot(['enron'], part='queries')
-        plot(['enron'], True, 'queries')
+        plot(data, part='queries')
+        plot(data, True, 'queries')
     else:
-        plot(colors.keys(), part='files')
-        plot(colors.keys(), True, 'files')
+        data = ['enron', 'lucene', 'wiki']
 
-        plot(colors.keys(), part='queries')
-        plot(colors.keys(), True, 'queries')
+        plot(data, part='files')
+        plot(data, True, 'files')
+
+        plot(data, part='queries')
+        plot(data, True, 'queries')
